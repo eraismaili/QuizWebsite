26cb311df0a53af064e9a356edbbdf9e7667290c
nekko <93317112+ErezaLoshi@users.noreply.github.com>
2023-06-25 14:37:30 +0200

    added the quiz questions/ added some functions

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does “www” stand for in a website browser",
+        answer: "World Wide Web",
+        options: [
+            "A. World's widest web",
+            "B. Wonderful World of Webs",
+            "C. Wireless Widget Web",
+            "D. World Wide Web"
+        ]
+    },
+    {
+        numb: 2,
+        question: " What was the fastest growing web browser in 2020?",
+        answer: "Microsoft Edge",
+        options: [
+            "A. Microsoft Edge",
+            "B. Firefox",
+            "C. DuckDuckGo",
+            "D. Internet Explorer"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which model of computer is the best selling of all time?",
+        answer: "Commodore 64",
+        options: [
+            "A. Apple II",
+            "B. IBM PC (5150)",
+            "C. Commodore 64",
+            "D. Sinclair ZX Spectrum"
+        ]
+    },
+    {
+        numb: 4,
+        question: " Which company developed the first mobile phone?",
+        answer: "Motorola",
+        options: [
+            "A. Motorola",
+            "B. Nokia",
+            "C. Samsung",
+            "D. Apple"
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "What year was the oldest programming language still in use invented?",
+        answer: "1957",
+        options: [
+            "A. 1957",
+            "B. 1952",
+            "C. 1960",
+            "D. 1974"
+
+        ]
+    }
+]
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -19,5 +20,29 @@ continueBtn.onclick = () => {
     quizSection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
 
+    showQuestions(0);
+}
+let questionCount = 0;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    questionCount++;
+    showQuestions(questionCount)
+}
+const optionList = document.querySelector('.option-list');
+
+//mi marr pytjet edhe opsionet prej array
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+<div class="option"><span>${questions[index].options[1]}</span></div>
+<div class="option"><span>${questions[index].options[2]}</span></div>
+<div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
 }
